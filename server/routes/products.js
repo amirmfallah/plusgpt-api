a05b2d667672afd4775c9c2fdd41951d7870ebc6
Amir Mohammad Fallah <amirmfallah@yahoo.com>
2023-05-31 17:57:52 +0330

    products version control

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const { getProducts } = require('../../models/Product');
+const express = require("express");
+const { getProducts } = require("../../models/Product");
 const router = express.Router();
-
 
 router.get("/", async (req, res) => {
   try {
-    const products = await getProducts({});
+    const latest = process.env.PRODUCT_VERSION;
+    const products = await getProducts({ version: latest });
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json(error);
   }
-})
+});
 
 module.exports = router;
